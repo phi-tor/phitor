28a0d9c95495c4304eea9e6f8dea1fc095853546
Welpike <welpike@protonmail.com>
2024-02-24 19:38:19 +0100

    add user model + fix problem with db (sqlite, we will see if we pass to mysql later)

---
+++ apps/back/app/models/user.ts
@@ -0,0 +1,33 @@
+import { DateTime } from 'luxon'
+import { withAuthFinder } from '@adonisjs/auth'
+import hash from '@adonisjs/core/services/hash'
+import { compose } from '@adonisjs/core/helpers'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
+  uids: ['username', 'email'],
+  passwordColumnName: 'password',
+})
+
+export default class User extends compose(BaseModel, AuthFinder) {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare username: string
+
+  @column()
+  declare email: string
+
+  @column()
+  declare password: string
+
+  @column()
+  declare lang: string
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime | null
+}
